b75885520fe2d4b15e8c6e79829c1475ae73b32b
Zhuang Ya <zhuangya@gmail.com>
2013-12-21 16:42:26 +0800

    clothes mockup

--- frontend/app/scripts/controllers/new.js
+++ frontend/app/scripts/controllers/new.js
@@ -10,4 +10,13 @@ angular.module('frontendApp')
     $scope.nos = {
       avatar: 'http://placekitten.com/64/64'
     };
+    $scope.goods = [{
+      id: 1,
+      image: 'http://placekitten.com/300/300',
+      size: ['XL', 'L', 'M', 'S']
+    }, {
+      id: 2,
+      image: 'http://placekitten.com/400/340',
+      size: ['XL', 'L', 'M']
+    }]
   });
