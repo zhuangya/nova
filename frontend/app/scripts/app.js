ca6cc047c5cab461852ba543882cfffc1d4680ba
Zhuang Ya <zhuangya@gmail.com>
2013-12-22 01:14:56 +0800

    clothes detail page.

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -56,3 +56,33 @@ angular.module('frontendApp', [
       $rootScope.pageClass = current.$$route.pageClass || '';
     });
   });
+
+angular.module('frontendApp').value('CLOTHES', [{
+      id: 1,
+      image: 'images/tees/1.jpg',
+      size: ['XL', 'L', 'M', 'S']
+    }, {
+      id: 2,
+      image: 'images/tees/2.jpg',
+      size: ['XL', 'L', 'M']
+    }, {
+      id: 3,
+      image: 'images/tees/3.jpg',
+      size: ['XL', 'L', 'M']
+    }, {
+      id: 4,
+      image: 'images/tees/4.jpg',
+      size: ['XL', 'L', 'M']
+    }, {
+      id: 5,
+      image: 'images/tees/5.jpg',
+      size: ['XL', 'L', 'M']
+    }, {
+      id: 6,
+      image: 'images/tees/6.jpg',
+      size: ['XL', 'L', 'M']
+    }, {
+      id: 7,
+      image: 'images/tees/7.jpg',
+      size: ['XL', 'L', 'M']
+    }]);
--- frontend/app/scripts/controllers/new.js
+++ frontend/app/scripts/controllers/new.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('frontendApp')
-  .controller('NewCtrl', function ($scope) {
+  .controller('NewCtrl', function ($scope, CLOTHES) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -10,33 +10,5 @@ angular.module('frontendApp')
     $scope.nos = {
       avatar: 'http://placekitten.com/64/64'
     };
-    $scope.goods = [{
-      id: 1,
-      image: 'images/tees/1.jpg',
-      size: ['XL', 'L', 'M', 'S']
-    }, {
-      id: 2,
-      image: 'images/tees/2.jpg',
-      size: ['XL', 'L', 'M']
-    }, {
-      id: 3,
-      image: 'images/tees/3.jpg',
-      size: ['XL', 'L', 'M']
-    }, {
-      id: 4,
-      image: 'images/tees/4.jpg',
-      size: ['XL', 'L', 'M']
-    }, {
-      id: 5,
-      image: 'images/tees/5.jpg',
-      size: ['XL', 'L', 'M']
-    }, {
-      id: 6,
-      image: 'images/tees/6.jpg',
-      size: ['XL', 'L', 'M']
-    }, {
-      id: 7,
-      image: 'images/tees/7.jpg',
-      size: ['XL', 'L', 'M']
-    }]
+    $scope.goods = CLOTHES;
   });
