738be7d81b649b70da0a8c74c97abc46b07a3ee9
Zhuang Ya <zhuangya@gmail.com>
2014-01-03 01:36:32 +0800

    clothes admin page listing route.

--- admin/app/scripts/controllers/clothes.js
+++ admin/app/scripts/controllers/clothes.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('adminApp')
-  .controller('ClothesCtrl', function ($scope) {
+  .controller('ClothesCtrl', function ($scope, $routeParams) {
+    $scope.action = $routeParams.action || 'list';
     $scope.addClothes = function() {
       console.log($scope.clothes);
     };
