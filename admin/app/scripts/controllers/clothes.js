b3c3798d2964ad89d7f54993b7a6d9e1a5f87309
Zhuang Ya <zhuangya@gmail.com>
2014-01-03 01:50:15 +0800

    back button.

--- admin/app/scripts/controllers/clothes.js
+++ admin/app/scripts/controllers/clothes.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('adminApp')
-  .controller('ClothesCtrl', function ($scope, $routeParams) {
+  .controller('ClothesCtrl', function ($scope, $location, $routeParams) {
     $scope.action = $routeParams.action || 'list';
-    $scope.addClothes = function() {
+    $scope.addClothes = function () {
       console.log($scope.clothes);
     };
+    $scope.cancel = function () {
+      $location.path('clothes');
+    };
+
   });
