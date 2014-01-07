81f933574041ddd533a46db745157327100054a1
Zhuang Ya <zhuangya@gmail.com>
2014-01-08 00:07:57 +0800

    detail functions

--- frontend/app/scripts/controllers/detail.js
+++ frontend/app/scripts/controllers/detail.js
@@ -8,4 +8,17 @@ angular.module('frontendApp')
     $http.get(APIBASE + '/data/' + $scope.detailId).success(function(clothes) {
       $scope.clothes = clothes;
     });
+
+    $scope.addToCart = function (clothes) {
+
+      $http.post(APIBASE + '/api/cart', {
+        //TODO: fix this request payload.
+        name: clothes.name,
+        unit_price: clothes.price
+      }).success(function (resp) {
+      }).error(function (error) {
+      });
+
+    };
+
   });
