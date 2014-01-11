d8879dbd05cc97901fe0837eb141b33f7c63f8ad
Zhuang Ya <zhuangya@gmail.com>
2014-01-12 01:33:10 +0800

    weibo login directive

--- frontend/app/scripts/directives/weibologin.js
+++ frontend/app/scripts/directives/weibologin.js
@@ -3,7 +3,21 @@
 angular.module('frontendApp')
   .directive('weiboLogin', function () {
     return {
-      template: '<div><a href="http://benzex.com/auth/weibo">weibo login</a></div>',
-      restrict: 'A'
+      //template: '<a href="/auth/weibo">weibologin</a>{{msg.screen_name}}',
+      templateUrl: 'views/directives/weiboLogin.html',
+      restrict: 'A',
+      controller: function ($scope, $http, $location) {
+        $scope.logout = function() {
+          $http.post('/auth/logout').success(function () {
+            $location.path('/');
+          });
+        };
+        $http.get('/api/user').success(function(who) {
+          $scope.who= who.profile._json;
+          $scope.isAuth = true;
+        }).error(function(error) {
+          $scope.isAuth = false;
+        });
+      }
     };
   });
