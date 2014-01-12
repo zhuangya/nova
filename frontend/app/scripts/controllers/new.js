dcc495ba1091df6093a15271e6efe6536dc5a6d5
Zhuang Ya <zhuangya@gmail.com>
2014-01-13 00:30:09 +0800

    fix frontend clothes image path error
    change admin grunt testing server port.

--- frontend/app/scripts/controllers/new.js
+++ frontend/app/scripts/controllers/new.js
@@ -4,8 +4,9 @@ angular.module('frontendApp')
   .controller('NewCtrl', function ($scope, $http, $log) {
 
     $http.get('/data').success(function (product) {
+      console.log(product);
       $scope.goods = _.map(product, function(p) {
-        p.coverImage = ['/photo', p.id, p.cover_name].join('/');
+        p.coverImage = ['/data', p.id, p.cover_name].join('/');
         return p;
       });
     });
