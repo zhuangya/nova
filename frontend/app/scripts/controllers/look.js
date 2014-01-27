c252ed2c9ef6934a36c20602ada18ef8e3644bfe
Zhuang Ya <zhuangya@gmail.com>
2014-01-27 23:31:37 +0800

    fix built lookbook no picture bug

--- frontend/app/scripts/controllers/look.js
+++ frontend/app/scripts/controllers/look.js
@@ -4,15 +4,15 @@ angular.module('frontendApp')
   .controller('LookCtrl', function ($scope, $http, $q, $compile) {
 
     $scope.waterFall = [
-      { "height": 401, "width": 600, "src": "images/lb001.jpg" },
-      { "height": 899, "width": 600, "src": "images/lb003.jpg" },
-      { "height": 899, "width": 600, "src": "images/lb002.jpg" },
-      { "height": 900, "width": 600, "src": "images/lb004.jpg" },
-      { "height": 900, "width": 600, "src": "images/lb005.jpg" },
-      { "height": 401, "width": 600, "src": "images/lb008.jpg" },
-      { "height": 900, "width": 600, "src": "images/lb007.jpg" },
-      { "height": 900, "width": 600, "src": "images/lb006.jpg" },
-      { "height": 401, "width": 600, "src": "images/lb009.jpg" }
+      { "height": 401, "width": 600, "src": "images/tees/lb001.jpg" },
+      { "height": 899, "width": 600, "src": "images/tees/lb003.jpg" },
+      { "height": 899, "width": 600, "src": "images/tees/lb002.jpg" },
+      { "height": 900, "width": 600, "src": "images/tees/lb004.jpg" },
+      { "height": 900, "width": 600, "src": "images/tees/lb005.jpg" },
+      { "height": 401, "width": 600, "src": "images/tees/lb008.jpg" },
+      { "height": 900, "width": 600, "src": "images/tees/lb007.jpg" },
+      { "height": 900, "width": 600, "src": "images/tees/lb006.jpg" },
+      { "height": 401, "width": 600, "src": "images/tees/lb009.jpg" }
     ];
 
     $scope.showImg = function (img) {
