f7e74c30b731181d983b3873f5dedd7fc1e942b4
Zhuang Ya <zhuangya@gmail.com>
2014-02-07 21:36:30 +0800

    login and logout weibo account.

--- frontend/app/scripts/directives/usercard.js
+++ frontend/app/scripts/directives/usercard.js
@@ -8,11 +8,20 @@ angular.module('frontendApp')
       },
       templateUrl: 'views/directives/usercard.html',
       restrict: 'A',
-      controller: function ($scope, $http) {
+      controller: function ($scope, $location, $http) {
+        $scope.logout = function () {
+          smoke.confirm('确定登出当前微博帐号吗？', function (event) {
+            if (event) {
+              $http.get('/auth/logout').success(function (resp) {
+                $location.path('/');
+              })
+            }
+          })
+        };
+        
         $http.get('/api/user').success(function(user) {
           $scope.user = user.profile._json;
         }).error(function (error) {
-          console.log(error);
           if (error.errno === 403) {
             $scope.needLogin = true
           }
