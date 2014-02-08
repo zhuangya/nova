97ee46685140026781f965ba1cf2a53212ef228f
Zhuang Ya <zhuangya@gmail.com>
2014-02-09 06:02:39 +0800

    me page

--- frontend/app/scripts/controllers/me.js
+++ frontend/app/scripts/controllers/me.js
@@ -1,10 +1,9 @@
 'use strict';
 
 angular.module('frontendApp')
-  .controller('MeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MeCtrl', function ($scope, $http) {
+    $http.get('/api/cart').success(function (clothes) {
+      $scope.clothes = clothes;
+    });
+    
   });
