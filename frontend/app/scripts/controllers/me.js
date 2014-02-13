2701f2dbafbfa02bacb268a49ff5ecd21d2abb7a
Zhuang Ya <zhuangya@gmail.com>
2014-02-13 22:44:34 +0800

    fix #51

--- frontend/app/scripts/controllers/me.js
+++ frontend/app/scripts/controllers/me.js
@@ -2,11 +2,42 @@
 
 angular.module('frontendApp')
   .controller('MeCtrl', function ($scope, $http) {
-    $http.get('/api/cart').success(function (clothes) {
-      $scope.cart = clothes;
-      $scope.hasClothes = !!clothes.length;
-      $scope.total = _.reduce(clothes, function (total, c) {
-        return total + c.count * c.unit_price;
+
+    function parseClothesName (name) {
+      var deferred = $q.defer();
+      name.replace(/(\w+)\/(\w+)\/(\w+)\/(\w+)/, function(match, category, name, variant, size) {
+        deferred.resolve({
+          category : category,
+          name : name,
+          variant : variant,
+          size : size
+        });
+      });
+
+      return deferred.promise;
+    };
+
+    $scope.updateTotal = function () {
+      $scope.total = _.reduce($scope.cart, function (price, c) {
+        if (c.count < 0) c.count = 0;
+        return price + c.unit_price * c.count;
       }, 0);
+    };
+
+    $http.get('/api/cart').success(function (clothes) {
+
+
+      $scope.cart = _.map(cart, function (item) {
+        parseClothesName(item.name).then(function (parsed) {
+          item._name = item.name;
+          angular.extend(item, parsed);
+        });
+        return item;
+      });
+      $scope.updateTotal();
+
+
+      $scope.hasClothes = !!clothes.length;
+
     });
   });
