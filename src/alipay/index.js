b29d136218cadf3dd2f5f86bcff182d24d2f0bcb
Bearice Ren <bearice@gmail.com>
2014-02-17 10:23:59 +0800

    fixes alipay return url malformed

--- src/alipay/index.js
+++ src/alipay/index.js
@@ -2,6 +2,6 @@ var Alipay = require('./lib/alipay').Alipay;
 var config = require('../../config');
 
 config_a = config.alipay;
-config_a.host = config.hostname;
+config_a.host = 'http://'+config.hostname;
 
 module.exports = new Alipay(config_a);
