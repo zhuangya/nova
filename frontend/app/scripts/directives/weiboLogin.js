c35f99a0b19c3de97f113b084fb7d4731556975d
Zhuang Ya <zhuangya@gmail.com>
2014-02-20 01:31:16 +0800

    Register and Login UI
    Close #52, Refs #48

--- frontend/app/scripts/directives/weiboLogin.js
+++ frontend/app/scripts/directives/weiboLogin.js
@@ -3,10 +3,15 @@
 angular.module('frontendApp')
   .directive('weiboLogin', function () {
     return {
-      //template: '<a href="/auth/weibo">weibologin</a>{{msg.screen_name}}',
       templateUrl: 'views/directives/weiboLogin.html',
       restrict: 'A',
       controller: function ($scope, $http, $location) {
+        $scope.showBox = false;
+
+        $scope.toggleBox = function () {
+          $scope.showBox = !$scope.showBox;
+        };
+
         $scope.logout = function() {
           $http.get('/auth/logout').success(function () {
             window.location = '/';
