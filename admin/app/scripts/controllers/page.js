dd36d24f02ac14fd7053235c64254896bb8c36c5
Zhuang Ya <zhuangya@gmail.com>
2014-03-07 01:43:06 +0800

    upload frontend logic
    refs #71

--- admin/app/scripts/controllers/page.js
+++ admin/app/scripts/controllers/page.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('adminApp')
-  .controller('PageCtrl', function ($scope) {
+  .controller('PageCtrl', function ($scope, $http, $upload) {
     $scope.pages = [{
       display_name: '首页',
       name: 'homepage'
@@ -10,8 +10,41 @@ angular.module('adminApp')
       name: 'detail'
     }];
 
+    $scope.onFileSelect = function(files) {
+      _.each(files, function(file) {
+      $scope.upload = $upload.upload({
+        url: '/api/admin/bg',
+        data: {
+          name: 'hell'
+        },
+        file: file,
+        fileFormDataName: 'background'
+      }).success(function(data) {
+        console.log(data);
+        console.log('bg uploaded');
+      });
+      });
+    };
+
     $scope.uploadBg = function (slug) {
       event.preventDefault();
-      console.log(slug);
+      $http({
+        method: 'POST',
+        url: '/api/admin/bg',
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        data: {
+          background: $scope.background,
+          name: slug
+        },
+        transformRequest: function(data) {
+          var fd = new FormData();
+          angular.forEach(data, function(value, key) {
+            fd.append(key, value);
+          });
+          return fd;
+        }
+      });
     };
   });
