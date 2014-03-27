37d9d29ed83e1a8b413eacb6a03968ae0af2e8fb
Zhuang Ya <zhuangya@gmail.com>
2014-03-28 00:22:59 +0800

    custom subtitle and about us wording frontend

--- frontend/app/scripts/controllers/c.js
+++ frontend/app/scripts/controllers/c.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('frontendApp')
-  .controller('CCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CCtrl', function ($scope, $http) {
+    $http.get('/api/wording', {
+      cache: true
+    }).success(function (resp) {
+      console.log(resp);
+      $scope.about = resp.about;
+      $scope.contact = resp.contact;
+    });
   });
--- frontend/app/scripts/controllers/new.js
+++ frontend/app/scripts/controllers/new.js
@@ -3,6 +3,11 @@
 angular.module('frontendApp')
   .controller('NewCtrl', function ($scope, $http, $log) {
 
+    $http.get('/api/wording', {
+      cache: true
+    }).success(function (resp) {
+      $scope.subtitle = resp.subtitle;
+    });
     $http.get('/data').success(function (product) {
       $scope.goods = _.map(product, function(p) {
         p.coverImage = ['/data', p.id, p.cover_name].join('/');
