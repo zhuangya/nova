7613709989a8021f18736b50332c466fd0a364e1
Zhuang Ya <zhuangya@gmail.com>
2014-04-10 02:32:10 +0800

    chagne look page images

--- frontend/app/scripts/controllers/look.js
+++ frontend/app/scripts/controllers/look.js
@@ -4,15 +4,7 @@ angular.module('frontendApp')
   .controller('LookCtrl', function ($scope, $http, $q, $compile) {
 
     $scope.waterFall = [
-      { "height": 401, "width": 600, "src": "images/tees/lb001.jpg" },
-      { "height": 899, "width": 600, "src": "images/tees/lb003.jpg" },
-      { "height": 899, "width": 600, "src": "images/tees/lb002.jpg" },
-      { "height": 900, "width": 600, "src": "images/tees/lb004.jpg" },
-      { "height": 900, "width": 600, "src": "images/tees/lb005.jpg" },
-      { "height": 401, "width": 600, "src": "images/tees/lb008.jpg" },
-      { "height": 900, "width": 600, "src": "images/tees/lb007.jpg" },
-      { "height": 900, "width": 600, "src": "images/tees/lb006.jpg" },
-      { "height": 401, "width": 600, "src": "images/tees/lb009.jpg" }
+      { "height": 1000, "width": 1000, "src": "images/tees/lookbook.jpg" }
     ];
 
     $scope.showImg = function (img) {
